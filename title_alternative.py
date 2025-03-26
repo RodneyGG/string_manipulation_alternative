cd26447b2bb04029ea5b729190034d60d5001305
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 18:12:30 +0800

    print the text in title case

--- title_alternative.py
+++ title_alternative.py
@@ -4,6 +4,15 @@ Create a program that do the same functionality without using title() function.
 """
 
 #Ask the user to enter word/s
+sentence = input("Enter Word/s: ") 
 #Split the word
+words = sentence.split()
+
 #the first letter for each word must be Uppercase and the rest is lowercase
+title_case = []
+for word in words:
+    title_case.append(word[0].upper() + word[1:].lower())
+
 #print the word 
+for word in title_case:
+    print(word, end=" ")
