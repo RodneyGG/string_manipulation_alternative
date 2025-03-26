8907bbfa79cdd3f11cbf0ec71c14e3cf3aff8108
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 16:08:09 +0800

    remove leading spaces and print

--- lstrip_alternative.py
+++ lstrip_alternative.py
@@ -4,5 +4,10 @@ Create a program that do the same functionality without using lstrip() function.
 """
 
 #Ask the user to input name with leading spaces
+name = input("Enter Name: ")
 #Identify how long is the leading spaces
+space = 0
+while space < len(name) and name[space] == " ":
+    space += 1
 #print name without spaces
+print(name[space:])
