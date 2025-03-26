912543f3d5f3584072a87b3b975cfbde77617782
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 16:31:19 +0800

    pseudocode

---
+++ convert_lowercase.py
@@ -0,0 +1,8 @@
+"""
+Prog03. lower() converts all characters of the string into lower case. 
+Create a program that do the same functionality without using lower() function.
+"""
+
+#ask the user to input a word
+#convert the word into all lowercase
+#print the lowercase word
