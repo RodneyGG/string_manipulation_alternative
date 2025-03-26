f7fd80d35dd4fedcde5636c078368e12f5ad3108
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 16:38:56 +0800

    Improve readability

--- convert_lowercase.py
+++ convert_lowercase.py
@@ -5,8 +5,11 @@ Create a program that do the same functionality without using lower() function.
 
 #ask the user to input a word
 word = input("Enter Word:")
-#convert the word into all lowercase
+
+#This will store the lowercase character
 result = ""
+
+#convert the word into all lowercase
 for char in word:
     #convert in ascii code
     if 65 <= ord(char) <= 90:
@@ -15,5 +18,6 @@ for char in word:
     else:
         #print the same if already lowercase or a number
         result += char
+
 #print the lowercase word
 print(result)
