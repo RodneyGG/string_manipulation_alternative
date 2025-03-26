9d66f7fe3b34ef511e36c830ce586ae66751b363
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 17:23:55 +0800

    pseudocode

---
+++ center_alternative.py
@@ -0,0 +1,8 @@
+"""
+Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. 
+Create a program that do the same functionality without using center() function.
+"""
+
+#Ask the user to input a word
+#ask the user what is the desired width 
+#print the word and center it with respect to the width
