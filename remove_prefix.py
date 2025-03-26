b92911f736572502b5a9da88f8cf52f1dd7a1328
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 16:28:53 +0800

    remove prefix and print it

--- remove_prefix.py
+++ remove_prefix.py
@@ -4,5 +4,14 @@ Create a program that do the same functionality without using removeprefix() fun
 """
 
 #Ask the user to input a word
+word = input("Enter Word: ")
+#Ask for prefix
+prefix = input("Enter Prefix: ")
+
 #It will remove prefix it if matches the word
+if word[:len(prefix)] == prefix:
+    #remove the prefix
+    word = word[len(prefix):]
+    
 #print the word with out prefix
+print(word)
