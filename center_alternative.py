24afa17098c7907db7fbadb3ad8d2694399d9eee
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 17:36:11 +0800

    number validator

--- center_alternative.py
+++ center_alternative.py
@@ -3,6 +3,29 @@ Prog07. center() add space characters at the beginning and at the end of the str
 Create a program that do the same functionality without using center() function.
 """
 
+#make a function that center it 
+def maangas_na_center(word, width):
+    if len(word) >= width:
+        return word
+    else:
+        space = (width - len(word)) // 2
+        return " " * space + word + " " * space 
+    
+#Number VAlidator  
+def valid_num(msg):
+    while True:
+        num = input(msg)
+        try:
+            return int(num)
+        except:
+            print("Only type numbers")
+            continue
+        
 #Ask the user to input a word
-#ask the user what is the desired width 
+word = input("Enter Word: ")
+#ask the user what is the desired width
+width = valid_num("Enter Width")
+
 #print the word and center it with respect to the width
+centered_word = maangas_na_center(word, width) 
+print(centered_word)
