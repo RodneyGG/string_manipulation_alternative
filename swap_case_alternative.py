b83e014b9d9d3ae603849b586fa9c2c9534c9d1f
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 17:42:58 +0800

    pseudocode

---
+++ swap_case_alternative.py
@@ -0,0 +1,8 @@
+"""
+Prog08. swapcase() reverse the casing of each of the character of the string. 
+Create a program that do the same functionality without using swapcase() function.
+"""
+
+#Ask the user to input a word/s
+#check if the word is upper or lower and convert it to the other form
+#print the result
