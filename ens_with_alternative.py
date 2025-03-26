c1dcc25733c914ccf4232f389a51aba3d421c931
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 16:53:44 +0800

    pseudocode

---
+++ ens_with_alternative.py
@@ -0,0 +1,8 @@
+"""
+Prog05. endswith() check if the string end part matches the function parameter. 
+Create a program that do the same functionality without using endswith() function.
+"""
+
+#ask the user to input a word
+#identify if the word end with the chosen word
+#print true or false
