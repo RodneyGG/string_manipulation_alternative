4933150ca52a07d580162112e421b2f952486381
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 18:32:41 +0800

    fix bugs and add option to quit

--- is_upper_alternative.py
+++ is_upper_alternative.py
@@ -2,6 +2,15 @@
 Prog04. isupper() check if all characters of the string is on upper case. 
 Create a program that do the same functionality without using isupper() function.
 """
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").strip().lower()
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
 
 #make a function identify if the string is on uppercase
 def check_upper(msg):
@@ -9,11 +18,14 @@ def check_upper(msg):
     for charac in word:
         if 97 <= ord(charac) <= 122:
             return False
-        else:
-            return True
+    return True
 
-#ask the user to input a word 
-word = check_upper("Enter Word: ")
+try_again = True
+while try_again:
+    #ask the user to input a word 
+    word = check_upper("Enter Word: ")
+        
+    #print true if uppercase else false
+    print("Uppercase:",word)
     
-#print true if uppercase else false
-print("Uppercase:",word)
+    try_again = ask_quit()
