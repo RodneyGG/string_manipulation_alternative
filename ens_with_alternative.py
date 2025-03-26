3d0fa9c998ec79ae97507229a43d2705e835b4a1
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 16:59:31 +0800

    Check if it ends the same

--- ens_with_alternative.py
+++ ens_with_alternative.py
@@ -4,5 +4,13 @@ Create a program that do the same functionality without using endswith() functio
 """
 
 #ask the user to input a word
+word = input("Enter Word: ")
+ending_word = input("Enter the ending word: ")
 #identify if the word end with the chosen word
+ends_with = True
+if word[-len(ending_word):] == ending_word:
+    ends_with = True
+else:
+    ends_with = False
 #print true or false
+print(ends_with)
