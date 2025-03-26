047901a9939f94a78adb849c2f3d2d55450229df
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 16:48:05 +0800

    identify if uppercase

--- is_upper_alternative.py
+++ is_upper_alternative.py
@@ -3,6 +3,17 @@ Prog04. isupper() check if all characters of the string is on upper case.
 Create a program that do the same functionality without using isupper() function.
 """
 
+#make a function identify if the string is on uppercase
+def check_upper(msg):
+    word = input(msg)
+    for charac in word:
+        if 97 <= ord(charac) <= 122:
+            return False
+        else:
+            return True
+
 #ask the user to input a word 
-#identify if the string is on uppercase
+word = check_upper("Enter Word: ")
+    
 #print true if uppercase else false
+print("Uppercase:",word)
