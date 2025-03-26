a085cda1aa9d29a4e8fe5c73b0cb05aaa6dd88ad
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 17:07:01 +0800

    pseudocode

---
+++ ljust_alternative.py
@@ -0,0 +1,8 @@
+"""
+Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. 
+Create a program that do the same functionality without using ljust() function.
+"""
+
+#Ask the user to enter a word
+#ASk the user what is the desired adjust
+#print word with the desired adjust
