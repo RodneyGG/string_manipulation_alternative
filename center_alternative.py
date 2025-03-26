0520dbb51769f95f33c90c17ed2f5c6ecb770a39
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 18:40:18 +0800

    added an option to quit

--- center_alternative.py
+++ center_alternative.py
@@ -3,6 +3,17 @@ Prog07. center() add space characters at the beginning and at the end of the str
 Create a program that do the same functionality without using center() function.
 """
 
+#make a function to ask the user to quit
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").strip().lower()
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
+
 #make a function that center it 
 def maangas_na_center(word, width):
     if len(word) >= width:
@@ -20,12 +31,17 @@ def valid_num(msg):
         except:
             print("Only type numbers")
             continue
-        
-#Ask the user to input a word
-word = input("Enter Word: ")
-#ask the user what is the desired width
-width = valid_num("Enter Width")
 
-#print the word and center it with respect to the width
-centered_word = maangas_na_center(word, width) 
-print(centered_word)
+try_again = True
+while try_again:        
+    #Ask the user to input a word
+    word = input("Enter Word: ")
+    #ask the user what is the desired width
+    width = valid_num("Enter Width: ")
+
+    #print the word and center it with respect to the width
+    centered_word = maangas_na_center(word, width) 
+    print(centered_word)
+    
+    #ask user to quit
+    try_again = ask_quit()
