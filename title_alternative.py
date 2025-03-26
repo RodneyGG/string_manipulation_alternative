69ab22fa709d31b9887cf3fe42d7a307f62314d0
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 18:05:59 +0800

    pseudocode

---
+++ title_alternative.py
@@ -0,0 +1,9 @@
+"""
+Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. 
+Create a program that do the same functionality without using title() function.
+"""
+
+#Ask the user to enter word/s
+#Split the word
+#the first letter for each word must be Uppercase and the rest is lowercase
+#print the word 
