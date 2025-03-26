13d54a642c100b14317e512ff056f9bbdfc9aa6f
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 16:37:22 +0800

    print in lowercase using ascii

--- convert_lowercase.py
+++ convert_lowercase.py
@@ -4,5 +4,16 @@ Create a program that do the same functionality without using lower() function.
 """
 
 #ask the user to input a word
+word = input("Enter Word:")
 #convert the word into all lowercase
+result = ""
+for char in word:
+    #convert in ascii code
+    if 65 <= ord(char) <= 90:
+        #add 32 to find its lowercase equevalent
+        result += chr(ord(char) + 32) 
+    else:
+        #print the same if already lowercase or a number
+        result += char
 #print the lowercase word
+print(result)
