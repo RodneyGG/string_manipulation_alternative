6a1dc28e03c57a3b40f615d019f6f535099199d8
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 18:28:11 +0800

    add option to quit

--- convert_lowercase.py
+++ convert_lowercase.py
@@ -3,21 +3,35 @@ Prog03. lower() converts all characters of the string into lower case.
 Create a program that do the same functionality without using lower() function.
 """
 
-#ask the user to input a word
-word = input("Enter Word:")
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").strip().lower()
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
 
-#This will store the lowercase character
-result = ""
+try_again = True
+while try_again:
+    #ask the user to input a word
+    word = input("Enter Word:")
 
-#convert the word into all lowercase
-for char in word:
-    #convert in ascii code
-    if 65 <= ord(char) <= 90:
-        #add 32 to find its lowercase equevalent
-        result += chr(ord(char) + 32) 
-    else:
-        #print the same if already lowercase or a number
-        result += char
+    #This will store the lowercase character
+    result = ""
 
-#print the lowercase word
-print(result)
+    #convert the word into all lowercase
+    for char in word:
+        #convert in ascii code
+        if 65 <= ord(char) <= 90:
+            #add 32 to find its lowercase equevalent
+            result += chr(ord(char) + 32) 
+        else:
+            #print the same if already lowercase or a number
+            result += char
+
+    #print the lowercase word
+    print(result)
+    
+    try_again = ask_quit()
