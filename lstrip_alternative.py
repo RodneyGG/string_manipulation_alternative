7cad9be9e178084accbf3fa200d04aa54270b269
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 16:15:15 +0800

    Added a function for readbility

--- lstrip_alternative.py
+++ lstrip_alternative.py
@@ -3,11 +3,19 @@ Prog01. lstrip() remove the space characters at the beginning of the string.
 Create a program that do the same functionality without using lstrip() function.
 """
 
+#Count the spaces
+def count_space(msg):
+    name = input(msg)
+    #Identify how long is the leading spaces
+    space = 0
+    while space < len(name) and name[space] == " ":
+        space += 1
+    #maangas
+    return name[space:]
+
+
 #Ask the user to input name with leading spaces
-name = input("Enter Name: ")
-#Identify how long is the leading spaces
-space = 0
-while space < len(name) and name[space] == " ":
-    space += 1
+name = count_space("Enter Name: ")
+
 #print name without spaces
-print(name[space:])
+print(name)
