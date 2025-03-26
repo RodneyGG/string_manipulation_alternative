a2e111767911728730b0e610f5b04d7005b67a64
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 16:42:10 +0800

    pseudocode

---
+++ is_upper_alternative.py
@@ -0,0 +1,8 @@
+"""
+Prog04. isupper() check if all characters of the string is on upper case. 
+Create a program that do the same functionality without using isupper() function.
+"""
+
+#ask the user to input a word 
+#identify if the string is on uppercase
+#print true if uppercase else false
