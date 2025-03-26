6daee7af09d78d922f770cec95a749e6eabc5ca4
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 17:20:33 +0800

    added a number validator

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -9,12 +9,22 @@ def make_space(word, desired_adjust):
         return word + " " * (desired_adjust - len(word))
     else:
         return word
-    
+
+#added a function so that when user enter letter it will not break    
+def valid_num(msg):
+    while True:
+        num = input(msg)
+        try:
+            return int(num)
+        except:
+            print("Only type numbers")
+            continue
+
 
 #Ask the user to enter a word
 word = input("Enter Word: ")
 #ASk the user what is the desired adjust
-desired_adjust = int(input("Enter desired adjust: "))
+desired_adjust = valid_num("Enter desired adjust: ")
 
 #print word with the desired adjust
 print(make_space(word, desired_adjust))
