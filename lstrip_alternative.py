a05fbfaa0bcd78ef8a4013778db078dd28f3b3ff
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 16:04:05 +0800

    pseudocode

---
+++ lstrip_alternative.py
@@ -0,0 +1,8 @@
+"""
+Prog01. lstrip() remove the space characters at the beginning of the string. 
+Create a program that do the same functionality without using lstrip() function.
+"""
+
+#Ask the user to input name with leading spaces
+#Identify how long is the leading spaces
+#print name without spaces
