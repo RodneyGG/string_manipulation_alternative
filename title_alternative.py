14664de670ccbf5494c83bf1fdb836dd2a40f56d
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 18:46:55 +0800

    added an option to as the user to quit

--- title_alternative.py
+++ title_alternative.py
@@ -3,16 +3,33 @@ Prog10. title() makes all first letter of each word in the string, capital lette
 Create a program that do the same functionality without using title() function.
 """
 
-#Ask the user to enter word/s
-sentence = input("Enter Word/s: ") 
-#Split the word
-words = sentence.split()
+#ask user to quit
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").strip().lower()
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
 
-#the first letter for each word must be Uppercase and the rest is lowercase
-title_case = []
-for word in words:
-    title_case.append(word[0].upper() + word[1:].lower())
+try_again = True
+while try_again:
+    #Ask the user to enter word/s
+    sentence = input("Enter Word/s: ") 
+    #Split the word
+    words = sentence.split()
 
-#print the word 
-for word in title_case:
-    print(word, end=" ")
+    #the first letter for each word must be Uppercase and the rest is lowercase
+    title_case = []
+    for word in words:
+        title_case.append(word[0].upper() + word[1:].lower())
+
+    #print the word 
+    for word in title_case:
+        print(word, end=" ")
+        
+    #ask the user to quit
+    print("")
+    try_again = ask_quit()
