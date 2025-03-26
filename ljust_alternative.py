886b316b1c1f6bf0349539838352051849a19b51
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 18:38:06 +0800

    added an option to quit

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -3,6 +3,18 @@ Prog06. ljust() add space characters at the end of the string to complete the nu
 Create a program that do the same functionality without using ljust() function.
 """
 
+#make a function that ask the user to quit
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").strip().lower()
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
+
+
 #make a function that add the justify
 def make_space(word, desired_adjust):
     if len(word) < desired_adjust:
@@ -20,12 +32,17 @@ def valid_num(msg):
             print("Only type numbers")
             continue
 
+try_again = True
+while try_again:
+    #Ask the user to enter a word
+    word = input("Enter Word: ")
+    #ASk the user what is the desired adjust
+    desired_adjust = valid_num("Enter desired adjust: ")
 
-#Ask the user to enter a word
-word = input("Enter Word: ")
-#ASk the user what is the desired adjust
-desired_adjust = valid_num("Enter desired adjust: ")
-
-#print word with the desired adjust
-left_justify = make_space(word, desired_adjust) 
-print(left_justify)
+    #print word with the desired adjust
+    left_justify = make_space(word, desired_adjust) 
+    print(left_justify)
+    
+    #ask the user to quit
+    try_again = ask_quit()
+    
