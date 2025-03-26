49840ddd0e6197f1c83b171affe202b99509dadb
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-26 17:45:53 +0800

    swap the casing of the character

--- swap_case_alternative.py
+++ swap_case_alternative.py
@@ -4,5 +4,19 @@ Create a program that do the same functionality without using swapcase() functio
 """
 
 #Ask the user to input a word/s
+word = input("Enter Word: ")
+
+#make a variable to store the result
+result = ""
+
 #check if the word is upper or lower and convert it to the other form
+for charac in word:
+    if charac.isupper():
+        result += charac.lower()
+    elif charac.islower():
+        result += charac.upper()
+    else:
+        result += charac
+
 #print the result
+print(result)
