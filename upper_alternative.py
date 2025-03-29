9434db278ac9ff86c69f889ab1e6b7718002d7c6
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 11:27:02 +0800

    added an option to quit

--- upper_alternative.py
+++ upper_alternative.py
@@ -3,18 +3,34 @@ Prog03. upper() converts all characters of the string into upper case.
 Create a program that do the same functionality without using upper() function.
 """
 
-#ask the user to input word
-word = input("Enter Word: ")
+#function to ask user to quit
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").lower().strip()
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
 
-#concatenate characters here
-result = ""
+try_again = True
+while try_again:
+    #ask the user to input word
+    word = input("Enter Word: ")
 
-#convert to upper case using ascii
-for charac in word:
-    if 97 <= ord(charac) <= 122:
-        result += chr(ord(charac) - 32)
-    else:
-        result += charac
+    #concatenate characters here
+    result = ""
 
-#print all uppercase
-print(result)
+    #convert to upper case using ascii
+    for charac in word:
+        if 97 <= ord(charac) <= 122:
+            result += chr(ord(charac) - 32)
+        else:
+            result += charac
+
+    #print all uppercase
+    print(result)
+    
+    #ask user to quit
+    try_again = ask_quit()
