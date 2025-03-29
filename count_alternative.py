603679f98e499c98c07d9a21464011fceba1ffb6
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 13:46:17 +0800

    pseudocode

---
+++ count_alternative.py
@@ -0,0 +1,8 @@
+"""
+Prog08. count() return how many time the function parameter appear in the string. 
+Create a program that do the same functionality without using count() function.
+"""
+
+#ask user to input a text
+#ask user to input character or word it like to count
+#print the count
