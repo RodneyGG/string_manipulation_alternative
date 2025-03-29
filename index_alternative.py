23562a87d306a8f49f658513226b7c15748b5a11
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 14:15:10 +0800

    pseudocode

---
+++ index_alternative.py
@@ -0,0 +1,9 @@
+"""
+Prog09. index() return the first location of the function parameter in the string. 
+Create a program that do the same functionality without using index() function.
+"""
+
+#ask the user to input a text
+#ask the user to input tha character they want to find
+#find the first time the character was found
+#print the number
