71f0267b24edb51c03eb8f481e62bb5a6ed26e55
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 14:40:29 +0800

    same feature of r index

--- rindez_alternative.py
+++ rindez_alternative.py
@@ -5,6 +5,24 @@ without using rindex() function.
 """
 
 #ask user to input a text 
+text = input("Enter text: ")
+
 #ask user to input any character it wnt t find
+parameter = input("What character or word would you like to find?\n")
+
+#initialize values
+position = -1
+text_length = len(text)
+param_length = len(parameter)
+
 #find the charectes starting from the right
+for i in range(text_length - 1, -1, -1):
+    if text[i:i+param_length] == parameter:
+        position = i
+        break 
+    
 #print the postion
+if position == -1:
+    print("Character not in the string")
+else:
+    print(position)
