04ff62be5690ac8eeb4e831dc90eeba091d5d5f7
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 13:27:02 +0800

    pseudocode

---
+++ zfill_alternative.py
@@ -0,0 +1,9 @@
+"""
+Prog07. zfill() add zero characters at the beginning of the string to complete 
+the number of characters specifies in function parameter. 
+Create a program that do the same functionality without using zfill() function.
+"""
+
+#ask user to input words or numbers
+#ask user to input a parameter
+#print words or number with leading 0
