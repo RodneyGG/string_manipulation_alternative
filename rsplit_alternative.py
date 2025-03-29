cd1c42d91d9e542589975e65dbf3900174db042d
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 09:27:14 +0800

    pseudocode

---
+++ rsplit_alternative.py
@@ -0,0 +1,8 @@
+"""
+Prog01. rsplit() remove the space characters at the end of the string. 
+Create a program that do the same functionality without using rsplit() function.
+"""
+
+#Ask user to input a word/s
+#Remove all the trailing spaces
+#print the word without trailing spaces
