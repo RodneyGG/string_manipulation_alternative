e979f0b1814120cc1e9b1ad32e62342abb03d53a
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 14:25:20 +0800

    added an option to quit

--- index_alternative.py
+++ index_alternative.py
@@ -3,25 +3,41 @@ Prog09. index() return the first location of the function parameter in the strin
 Create a program that do the same functionality without using index() function.
 """
 
-#ask the user to input a text
-text = input("Enter text: ")
+#make a function that ask user to quit
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").lower().strip()
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
 
-#ask the user to input tha character they want to find
-parameter = input("What character or word would you like to find?\n")
+try_again = True
+while try_again:
+    #ask the user to input a text
+    text = input("Enter text: ")
 
-#initialize values
-position = -1
-text_length = len(text)
-param_length = len(parameter)
+    #ask the user to input tha character they want to find
+    parameter = input("What character or word would you like to find?\n")
 
-#find the first time the character was found
-for i in range(text_length - param_length + 1):
-    if text[i:i+param_length] == parameter:
-        position = i
-        break
+    #initialize values
+    position = -1
+    text_length = len(text)
+    param_length = len(parameter)
 
-#print the number
-if position == -1:
-    print(f"There is no {parameter} in {text}")
-else:
-    print(position)
+    #find the first time the character was found
+    for i in range(text_length - param_length + 1):
+        if text[i:i+param_length] == parameter:
+            position = i
+            break
+
+    #print the number
+    if position == -1:
+        print(f"There is no {parameter} in {text}")
+    else:
+        print(position)
+
+    #ask user to quit the program
+    try_again = ask_quit()
