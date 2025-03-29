406debb19430467a8d3778225e43618854cde82e
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 12:51:18 +0800

    added a exit option

--- start_with_alternative.py
+++ start_with_alternative.py
@@ -2,17 +2,36 @@
 Prog05. startswith() check if the string beginning part matches the function parameter. 
 Create a program that do the same functionality without using startswith() function.
 """
+#make a function that ask user to quit
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").lower().strip()
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
 
-#ask user to enter a word
-word = input("Enter Word: ")
-#ask user to input any character
-start_charac = input("Enter characters: ")
-#initialize boolean value
-start_with = True
-#check if the the condition is True
-if word[:len(start_charac)] == start_charac:
+try_again = True
+while try_again:
+    #ask user to enter a word
+    word = input("Enter Word: ")
+
+    #ask user to input any character
+    start_charac = input("Enter characters: ")
+
+    #initialize boolean value
     start_with = True
-else:
-    start_with = False
-#print if true or false
-print(start_with)
+
+    #check if the the condition is True
+    if word[:len(start_charac)] == start_charac:
+        start_with = True
+    else:
+        start_with = False
+
+    #print if true or false
+    print(start_with)
+    
+    #ask user to quit
+    try_again = ask_quit()
