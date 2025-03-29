44b155bad9c737d1046ed0947ce1f58a7057cb4f
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 14:46:17 +0800

    added an option to quit the program

--- rindez_alternative.py
+++ rindez_alternative.py
@@ -4,25 +4,41 @@ starting from the last character. Create a program that do the same functionalit
 without using rindex() function.
 """
 
-#ask user to input a text 
-text = input("Enter text: ")
+#make a function that ask user to quit
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").lower().strip()
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
 
-#ask user to input any character it wnt t find
-parameter = input("What character or word would you like to find?\n")
+try_again = True
+while try_again:
+    #ask user to input a text 
+    text = input("Enter text: ")
 
-#initialize values
-position = -1
-text_length = len(text)
-param_length = len(parameter)
+    #ask user to input any character it wnt t find
+    parameter = input("What character or word would you like to find?\n")
 
-#find the charectes starting from the right
-for i in range(text_length - 1, -1, -1):
-    if text[i:i+param_length] == parameter:
-        position = i
-        break 
-    
-#print the postion
-if position == -1:
-    print("Character not in the string")
-else:
-    print(position)
+    #initialize values
+    position = -1
+    text_length = len(text)
+    param_length = len(parameter)
+
+    #find the charectes starting from the right
+    for i in range(text_length - 1, -1, -1):
+        if text[i:i+param_length] == parameter:
+            position = i
+            break 
+        
+    #print the postion
+    if position == -1:
+        print("Character not in the string")
+    else:
+        print(position)
+        
+    #ask user to quit 
+    try_again = ask_quit()
