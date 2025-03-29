52b54c25bdea2a22ad15b028ac72c1486193231c
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 11:45:28 +0800

    identify if lowercase

--- is_lower_alternative.py
+++ is_lower_alternative.py
@@ -4,5 +4,16 @@ Create a program that do the same functionality without using islower() function
 """
 
 #ask user to input a word
+word = input("Enter Word: ")
+
+#initialize as boolean Value
+lower_case = True
+
 #check if the all of the characters are in lowercase using ascii
+for charac in word:
+    if 65 <= ord(charac)>= 90:
+        lower_case = False
+        break
+    
 #print True or False
+print(lower_case)
