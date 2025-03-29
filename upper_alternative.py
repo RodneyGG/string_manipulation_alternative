0c2ac6bddbf39856422d1806277d57dd6ad60277
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 11:25:13 +0800

    print in all uppercase

--- upper_alternative.py
+++ upper_alternative.py
@@ -4,5 +4,17 @@ Create a program that do the same functionality without using upper() function.
 """
 
 #ask the user to input word
+word = input("Enter Word: ")
+
+#concatenate characters here
+result = ""
+
 #convert to upper case using ascii
+for charac in word:
+    if 97 <= ord(charac) <= 122:
+        result += chr(ord(charac) - 32)
+    else:
+        result += charac
+
 #print all uppercase
+print(result)
