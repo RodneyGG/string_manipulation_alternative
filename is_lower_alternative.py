9492b572d4f88d2c06bf83d737a99c136120852f
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 11:41:25 +0800

    psuedocode

---
+++ is_lower_alternative.py
@@ -0,0 +1,8 @@
+"""
+Prog04. islower() check if all characters of the string is on lower case. 
+Create a program that do the same functionality without using islower() function.
+"""
+
+#ask user to input a word
+#check if the all of the characters are in lowercase using ascii
+#print True or False
