c0a2d2c55c0ea320a6b3b2b64b7b51dd7267d5d9
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 13:05:54 +0800

    added a number validator

--- rjust_alternative.py
+++ rjust_alternative.py
@@ -4,11 +4,19 @@ of characters specifies in function parameter. Create a program that do the same
 functionality without using rjust() function.
 """
 
+def valid_num(msg):
+    while True:    
+        num = input(msg)
+        try:
+            return int(num)
+        except:
+            print("Invalid Number")
+
 #Ask user to input word/s
 word = input("Enter Words: ")
 
 #ask user the width 
-width = int(input("Enter Width: "))
+width = valid_num("Enter Width: ")
 
 #print with the in respect with the width
 space = " " * (width - len(word))
