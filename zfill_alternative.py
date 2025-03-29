0441e873462154290e042febb022ff3d37ec4ca8
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 13:34:33 +0800

    Number validator that only accept whole number

--- zfill_alternative.py
+++ zfill_alternative.py
@@ -4,10 +4,19 @@ the number of characters specifies in function parameter.
 Create a program that do the same functionality without using zfill() function.
 """
 
+#number validator
+def valid_num(msg):
+    while True:    
+        num = input(msg)
+        try:
+            return int(num)
+        except:
+            print("Invalid Number.(Whole Number)")
+
 #ask user to input words or numbers
 characters = input("Enter Words or Number: ")
 #ask user to input a parameter
-parameter = int(input("Enter parameter: "))
+parameter = valid_num("Enter parameter: ")
 #print words or number with leading 0
 if len(characters) >= parameter:
     print(characters)
