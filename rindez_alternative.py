16ba8b43b685d8417df3109cad998087080710f4
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 14:34:28 +0800

    pseudocode

---
+++ rindez_alternative.py
@@ -0,0 +1,10 @@
+"""
+Prog10. rindex() return the first location of the function parameter in the string 
+starting from the last character. Create a program that do the same functionality 
+without using rindex() function.
+"""
+
+#ask user to input a text 
+#ask user to input any character it wnt t find
+#find the charectes starting from the right
+#print the postion
