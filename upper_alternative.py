34923b9a96bea67f1663f12a8923b626ce2e22bc
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 11:13:17 +0800

    pseudocode

---
+++ upper_alternative.py
@@ -0,0 +1,8 @@
+"""
+Prog03. upper() converts all characters of the string into upper case. 
+Create a program that do the same functionality without using upper() function.
+"""
+
+#ask the user to input word
+#convert to upper case using ascii
+#print all uppercase
