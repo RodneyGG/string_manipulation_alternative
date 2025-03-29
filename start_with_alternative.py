61705398d1fd1380e2b7b04af2f4add5844303de
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 12:43:57 +0800

    pseudocode

---
+++ start_with_alternative.py
@@ -0,0 +1,8 @@
+"""
+Prog05. startswith() check if the string beginning part matches the function parameter. 
+Create a program that do the same functionality without using startswith() function.
+"""
+
+#ask user to enter a word
+#ask user to input any character
+#check if the the condition is True
