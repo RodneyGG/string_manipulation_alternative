c6e45fe8f08006ff6f42fb3efe842e0bce3d9258
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-29 13:51:57 +0800

    count words

--- count_alternative.py
+++ count_alternative.py
@@ -4,5 +4,13 @@ Create a program that do the same functionality without using count() function.
 """
 
 #ask user to input a text
+text = input("Enter Text: ")
 #ask user to input character or word it like to count
+parameter = input("What character or word would you like to count?\n")
+#initialize count
+count = 0
+for i in range(len(text) - len(parameter) + 1):
+    if text[i:i+len(parameter)] == parameter:
+        count += 1 
 #print the count
+print(count)
